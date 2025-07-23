dfbcc881c4af351b96939317b8c46075c604fb70
romoboss <101649515+romoboss@users.noreply.github.com>
2025-07-23 16:30:09 +0200

    1.25
    Scavenger hunt completed

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const footerHTML = `
                 <a href="/aboutme"><p><b>Contact Me</b></p></a>
             </div>
             <p>Copyright &copy; 2024 Romoboss Games. All rights reserved.</p>
-            <p>Website Version 1.24</p>
+            <p>Website Version 1.25</p>
         </div>
     </div>
 `;
